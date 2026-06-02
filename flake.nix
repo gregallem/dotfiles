9606385e8d3079065f50a5bfc2f4118cfd444204
Greg Allen <greg.allen54@gmail.com>
2026-06-02 16:08:41 +1000

    Add files via upload
    Removed system data

--- flake.nix
+++ flake.nix
@@ -6,10 +6,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
